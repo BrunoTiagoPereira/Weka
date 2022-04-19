eb024d52f078fc6f98c8cb5cfbcb6ae81f3b0c80
Bruno Tiago <brunotiago2000@hotmail.com>
2022-04-19 13:01:19 -0300

    validations

--- src/com/codebind/App.java
+++ src/com/codebind/App.java
@@ -1,20 +1,82 @@
 package com.codebind;
 
+import com.codebind.DataSet.Dtos.HeartInstance;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class App extends JFrame {
     private static String filePath;
 
     public App(){
 
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                var errors = validateInputs();
+
+                boolean hasErrors = !isNullOrEmpty(errors);
+                if(hasErrors){
+                    JOptionPane.showMessageDialog(null,errors, "Erros", JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+
+                Weka dt = new Weka(filePath);
+
+                var instance = new HeartInstance();
+                instance.age = Double.parseDouble(ageTxt.getText());
+                instance.sex = Double.parseDouble(sexCmb.getSelectedItem().toString());
+                instance.cp = Double.parseDouble(cpCmb.getSelectedItem().toString());
+                instance.trtbps = Double.parseDouble(trtbpsTxt.getText());
+                instance.chol = Double.parseDouble(cholTxt.getText());
+                instance.fbs = Double.parseDouble(fbsCmb.getSelectedItem().toString());
+                instance.rest_ecg = Double.parseDouble(restEcgCmb.getSelectedItem().toString());
+                instance.thalach = Double.parseDouble(thalachTxt.getText());
+                instance.exng = Double.parseDouble(exngCmb.getSelectedItem().toString());
+                instance.oldpeak = Double.parseDouble(oldpeakTxt.getText());
+                instance.slp = Double.parseDouble(slpCmb.getSelectedItem().toString());
+                instance.caa = Double.parseDouble(caaCmb.getSelectedItem().toString());
+                instance.thall = Double.parseDouble(thalachTxt.getText());
+                //63,1,3,145,233,1,0,150,0,2.3,0,0,1
+
+                try{
+                    dt.readData();
+                    System.out.println( dt.J48(instance));
+                    System.out.println( dt.J48CrossEvaluation(instance));
+                    System.out.println( dt.IBK(instance));
+
+                }
+                catch (Exception ex){
+
+                    System.out.println(ex);
+                }
+
+            }
+        });
     }
+
     public JPanel MainPanel;
     private JLabel formTitle;
-    private  JTextField ageTxt;
-    private JTextField textField3;
-    private JTextField textField4;
-    private JTextField textField5;
-    private JComboBox comboBox1;
+    private JTextField ageTxt;
+    private JTextField trtbpsTxt;
+    private JTextField cholTxt;
+    private JComboBox sexCmb;
+    private JComboBox cpCmb;
+    private JComboBox fbsCmb;
+    private JComboBox restEcgCmb;
+    private JTextField thalachTxt;
+    private JComboBox exngCmb;
+    private JTextField oldpeakTxt;
+    private JComboBox slpCmb;
+    private JComboBox caaCmb;
+    private JComboBox thallCmb;
+    private JButton submitButton;
 
 
     private static JPanel currentPanel;
@@ -30,11 +92,13 @@ public class App extends JFrame {
         f.setSize(1200, 720);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setVisible(true);
+
     }
+
 
     public static void main(String[] args){
         InitUiElements();
-
+        filePath = "C:\\Users\\bruno\\Downloads\\heart.arff";
         /*
         filePath = "C:\\Users\\bruno\\Downloads\\heart.arff";
         Weka dt = new Weka(filePath);
@@ -61,6 +125,75 @@ public class App extends JFrame {
     }
 
 
+    private String validateInputs(){
+
+        StringBuilder sb = new StringBuilder();
+        var txtFields = getTxtFields();
+
+        var emptyFields = txtFields
+                .stream()
+                .filter(f -> isNullOrEmpty(f.getText()))
+                .map(f-> f.getName())
+                .toArray()
+                ;
+
+        if(emptyFields.length > 0){
+            sb.append("Os campos informados não estão preenchidos: \n");
+            for (var fieldName: emptyFields) {
+                sb.append(fieldName + "\n");
+            }
+        }
+
+        var hasLettersFields = txtFields
+                .stream()
+                .filter(f -> !Arrays.stream(emptyFields).anyMatch(x -> x == f.getName()))
+                .filter(f -> hasLetters(f.getText()))
+                .map(f-> f.getName() + "\n")
+                .toArray()
+                ;
+
+        if(hasLettersFields.length > 0){
+            sb.append("\n Os campos informados não devem ter letras: \n");
+            for (var fieldName: hasLettersFields) {
+                sb.append(fieldName);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    private ArrayList<JTextField> getTxtFields(){
+
+        ArrayList<JTextField> txtFields = new ArrayList<>();
+
+        ageTxt.setName("Idade (Age)");
+        thalachTxt.setName("Máximo de ritmo do coração (Thalach)");
+        trtbpsTxt.setName("Pressão do sangue descansando (Trtbps)");
+        cholTxt.setName("Colesterol coletado (Chol)");
+        oldpeakTxt.setName("Depressão do segmento ST induzida (Oldpeak)");
+
+        txtFields.add(ageTxt);
+        txtFields.add(thalachTxt);
+        txtFields.add(trtbpsTxt);
+        txtFields.add(cholTxt);
+        txtFields.add(oldpeakTxt);
+
+        return txtFields;
+    }
+
+    private boolean isNullOrEmpty(String str){
+        return str == null || str.isBlank() || str.isEmpty();
+    }
+    private boolean hasLetters(String str){
+
+        String regex = "^\\d+(\\.\\d+)*$";
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(str);
+
+        boolean onlyNumbers = matcher.matches();
+
+        return !onlyNumbers;
+    }
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
